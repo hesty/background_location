75cc6a6bca045834b7f970e1720bedfe2a5d55b5
Cihat Karaboğa <hestu12@gmail.com>
2021-06-16 23:05:41 +0300

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,60 +30,63 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Background Location Service'),
         ),
         body: Center(
-          child: ListView(
-            children: <Widget>[
-              locationData('Latitude: ' + latitude),
-              locationData('Longitude: ' + longitude),
-              locationData('Altitude: ' + altitude),
-              locationData('Accuracy: ' + accuracy),
-              locationData('Bearing: ' + bearing),
-              locationData('Speed: ' + speed),
-              locationData('Time: ' + time),
-              ElevatedButton(
-                  onPressed: () async {
-                    await BackgroundLocation.setAndroidNotification(
-                      title: 'Background service is running',
-                      message: 'Background location in progress',
-                      icon: '@mipmap/ic_launcher',
-                    );
-                    //await BackgroundLocation.setAndroidConfiguration(1000);
-                    await BackgroundLocation.startLocationService(
-                        distanceFilter: 20);
-                    BackgroundLocation.getLocationUpdates((location) {
-                      setState(() {
-                        latitude = location.latitude.toString();
-                        longitude = location.longitude.toString();
-                        accuracy = location.accuracy.toString();
-                        altitude = location.altitude.toString();
-                        bearing = location.bearing.toString();
-                        speed = location.speed.toString();
-                        time = DateTime.fromMillisecondsSinceEpoch(
-                                location.time!.toInt())
-                            .toString();
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: <Widget>[
+                locationData('Latitude: ' + latitude),
+                locationData('Longitude: ' + longitude),
+                locationData('Altitude: ' + altitude),
+                locationData('Accuracy: ' + accuracy),
+                locationData('Bearing: ' + bearing),
+                locationData('Speed: ' + speed),
+                locationData('Time: ' + time),
+                ElevatedButton(
+                    onPressed: () async {
+                      await BackgroundLocation.setAndroidNotification(
+                        title: 'Background service is running',
+                        message: 'Background location in progress',
+                        icon: '@mipmap/ic_launcher',
+                      );
+                      //await BackgroundLocation.setAndroidConfiguration(1000);
+                      await BackgroundLocation.startLocationService(
+                          distanceFilter: 20);
+                      BackgroundLocation.getLocationUpdates((location) {
+                        setState(() {
+                          latitude = location.latitude.toString();
+                          longitude = location.longitude.toString();
+                          accuracy = location.accuracy.toString();
+                          altitude = location.altitude.toString();
+                          bearing = location.bearing.toString();
+                          speed = location.speed.toString();
+                          time = DateTime.fromMillisecondsSinceEpoch(
+                                  location.time!.toInt())
+                              .toString();
+                        });
+                        print('''\n
+                          Latitude:  $latitude
+                          Longitude: $longitude
+                          Altitude: $altitude
+                          Accuracy: $accuracy
+                          Bearing:  $bearing
+                          Speed: $speed
+                          Time: $time
+                        ''');
                       });
-                      print('''\n
-                        Latitude:  $latitude
-                        Longitude: $longitude
-                        Altitude: $altitude
-                        Accuracy: $accuracy
-                        Bearing:  $bearing
-                        Speed: $speed
-                        Time: $time
-                      ''');
-                    });
-                  },
-                  child: Text('Start Location Service')),
-              ElevatedButton(
-                  onPressed: () {
-                    BackgroundLocation.stopLocationService();
-                  },
-                  child: Text('Stop Location Service')),
-              ElevatedButton(
-                  onPressed: () {
-                    getCurrentLocation();
-                  },
-                  child: Text('Get Current Location')),
-            ],
+                    },
+                    child: Text('Start Location Service')),
+                ElevatedButton(
+                    onPressed: () {
+                      BackgroundLocation.stopLocationService();
+                    },
+                    child: Text('Stop Location Service')),
+                ElevatedButton(
+                    onPressed: () {
+                      getCurrentLocation();
+                    },
+                    child: Text('Get Current Location')),
+              ],
+            ),
           ),
         ),
       ),
